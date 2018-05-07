f98dc05573773239b2ebd5da124360f80159004a
Vajid Kagdi <vajid9@gmail.com>
2018-05-06 22:28:21 -0700

    CORS issue and static files

--- FreelancerUsingSpringBoot/src/main/java/com/freelancer/Main.java
+++ FreelancerUsingSpringBoot/src/main/java/com/freelancer/Main.java
@@ -3,10 +3,25 @@ package com.freelancer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Main {
     public static void main(String [] args){
         SpringApplication.run(Main.class,args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+            }
+        };
     }
 }
